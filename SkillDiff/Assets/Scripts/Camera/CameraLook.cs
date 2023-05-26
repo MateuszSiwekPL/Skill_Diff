9c6aa6038141c7078a3ed42dd9d0a110b99d08c7
MateuszSiwekPL <mateusz.siwek.pl@gmail.com>
2023-05-26 18:15:58 +0200

    Camera behaviour and test level

---
+++ SkillDiff/Assets/Scripts/Camera/CameraLook.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+   
+    PlayerInputs controlls;
+    [SerializeField] Transform player;
+
+
+    float sensitivity = 25f;
+    float rotationX = 0f;
+    float rotationY = 0f;
+    Vector2 look;
+    
+
+
+   private void Awake() 
+   {
+        controlls = new PlayerInputs();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+   }
+
+   private void Update() 
+   {
+        Looking();
+   }
+
+   private void Looking()
+   {
+        look = controlls.Player.Look.ReadValue<Vector2>();
+        float lookX = look.x * sensitivity * Time.deltaTime;
+        float lookY = look.y * sensitivity * Time.deltaTime;
+
+        rotationY += lookX;
+
+        rotationX -= lookY;
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+
+        transform.rotation = Quaternion.Euler(rotationX, rotationY, 0);
+        player.rotation = Quaternion.Euler(0, rotationY, 0);
+
+   }
+
+
+
+   private void OnEnable() {controlls.Enable();}
+
+   private void OnDisable() {controlls.Disable();}
+}
