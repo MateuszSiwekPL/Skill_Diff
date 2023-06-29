89fbe9c2836806a9cc57536b3b0986e89109deda
MateuszSiwekPL <mateusz.siwek.pl@gmail.com>
2023-06-29 23:14:09 +0200

    kill mechanic

--- SkillDiff_clone_1/Assets/Scripts/GameplayLogic/TeamDeathMatch.cs
+++ SkillDiff_clone_1/Assets/Scripts/GameplayLogic/TeamDeathMatch.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 
-public class TeamDeathMatch : NetworkBehaviour
+public class TeamDeathMatch : NetworkBehaviour, IKillable
 {
     [Header("Player Id")]
     public string team;
@@ -11,6 +11,9 @@ public class TeamDeathMatch : NetworkBehaviour
     private Vector3 spawnPosition;
     private void Start() 
     {
+        if(!IsOwner && !IsServer)
+        gameObject.GetComponent<CapsuleCollider>().isTrigger = true;
+
         spawnPosition = GameObject.Find(team + "_" + id.ToString()).transform.position;
         StartCoroutine(Wait());
     }
@@ -28,5 +31,11 @@ public class TeamDeathMatch : NetworkBehaviour
         transform.position = spawnPosition;
         transform.LookAt(new Vector3(0, transform.position.y, 0));
         
+    }
+
+    public void Kill()
+    {
+        SpawningServerRpc();
+        Spawning();
     }
 }
